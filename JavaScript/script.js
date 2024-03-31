619ff545710e7a04c7ed84baf8feb902b629ad72
Sudhanshu-06 <sudhanshu767778@gmail.com>
2024-03-31 18:33:57 +0530

    push

---
+++ JavaScript/script.js
@@ -0,0 +1,76 @@
+// var x=6;
+// console.log(x);
+
+// var abc1= 5;
+// console.log(abc1);
+
+// var userName="Sudhanshu";
+// console.log(userName);
+
+// let arr1=["Aman",22,"Suman","true",45];
+// console.log(arr1);
+// console.log(arr1[2]);
+
+// User registration
+
+let userName="Sudhansu";
+let fullName="Sudhanshu Kumar"
+const eMail="Sudhansh788999@gmail.com"
+let phoneNumber="5748888888622"
+const pass="qwerty"
+console.table(typeof fullName);
+console.log("My user name is " +userName + " My mobile num is " + phoneNumber);
+
+// this type of way is writing is called tempalte string or interpolation and tempalte literal
+console.log(`
+My User name: ${userName},
+My Mobile Number is: ${phoneNumber}
+`)
+
+//Operators
+let a=24;
+let b=78;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a%b);
+console.log(a/b);
+
+
+let c=25;
+let d="5";
+let result= c+d;
+console.log(typeof result);
+console.log(result)
+
+
+let mrp=1999;
+let disPrice=999;
+console.log("The mrp of item is" + mrp);
+
+let dPrice=((mrp-disPrice)/mrp)*100;
+console.log("The Discount Percentage is" + dPrice);
+let roundPrice= Math.round(dPrice);
+console.log("The Discounted percentage is " + roundPrice);
+
+
+let ab=5;
+let abc=ab++
+console.log(abc);
+console.log(typeof abc)
+
+let bc=8;
+let bca=++bc
+console.log(bca);
+console.log(typeof bca)
+
+
+
+
+ 
+
+
+
+
+
+
